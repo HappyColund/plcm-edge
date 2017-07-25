0cec6ac94d238ea849d9e13ac7816884224d3edd
yzy_08_vip@163.com <yzy_08_vip@163.com>
2017-07-25 12:41:19 +0800

    初始化项目

---
+++ src/main/java/com/svocloud/plcmedge/App.java
@@ -0,0 +1,13 @@
+package com.svocloud.plcmedge;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
